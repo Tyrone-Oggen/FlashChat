3943ad8fb05565829aba1a1ce5b6389c082173f3
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-09-01 19:15:16 +0200

    New constants added to file for BrandColors, FStore and table cell content

--- Flash Chat/Constants.swift
+++ Flash Chat/Constants.swift
@@ -7,6 +7,23 @@
 //
 
 struct Constants {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
     static let registerToChatSegue = "RegisterToChat"
     static let loginToChatSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
 }
