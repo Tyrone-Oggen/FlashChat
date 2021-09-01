c138765e44fbb087f659913759c119d9c95ae23a
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-09-01 17:28:24 +0200

    User registration added to button using Auth.auth().createUser(withEmail: email, password: password)

--- Flash Chat/Controller/RegisterViewController.swift
+++ Flash Chat/Controller/RegisterViewController.swift
@@ -5,7 +5,9 @@
 //  Created by Tyrone Oggen on 2021/08/31.
 //  Copyright © 2021 Tyrone Oggen. All rights reserved.
 //
+
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -13,6 +15,16 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    //localizedDescription is used to translate it to the user's selected language
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
