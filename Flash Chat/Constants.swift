d9d64d4f0fbf72ff7f6f9c0dfd2baf9f7dbc0f08
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-09-01 18:34:40 +0200

    Constants file added to ensure string consistency

---
+++ Flash Chat/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flash Chat
+//
+//  Created by Tyrone Oggen on 2021/09/01.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    static let registerToChatSegue = "RegisterToChat"
+    static let loginToChatSegue = "LoginToChat"
+}
